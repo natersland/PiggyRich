e022aadacdf42e6f36e9c06b5c8f46e645a60edb
Akira Chenyavanij <natersland@gmail.com>
2023-02-20 01:50:43 +0700

    fix: compile error

--- app/src/main/java/com/natersfantasy/piggyrichrpg/util/UiEvent.kt
+++ app/src/main/java/com/natersfantasy/piggyrichrpg/util/UiEvent.kt
@@ -3,4 +3,8 @@ package com.natersfantasy.piggyrichrpg.util
 sealed class UiEvent {
     object PopBackStack: UiEvent()
     data class Navigate(val route: String): UiEvent()
+    data class ShowSnackbar(
+        val message: String,
+        val action: String? = null
+    ): UiEvent()
 }
